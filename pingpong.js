f5a9f4f1b2f631ec7df231d93110671b591b5fbf
fernando1171 <fernando.noqueli@escola.pr.gov.br>
2026-02-20 07:52:48 -0300

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,34 @@
+//Criar Bolinha
+let raio = 7.5;
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = raio * 2;
+
+//Velocidade da bolinha
+let velocidadeXbolinha = 6;
+let velocidadeYBolinha = 6;
+
+//Minha Raquete
+let xRaquete = 5;
+let yRaquete = 150; 
+
+// Raquete Oponente
+let xRaquete = 5;
+let yRaquete = 150;
+
+// Placar
+let XRaqueteOponente = 585;
+let YRaqueteOponente = 150;
+
+// Placar
+let meusPontos = 0;
+let pontosOponente = 0;
+
+function setup(){ 
+  createCanvas(600, 400)
+  
+}
+
+function draw(){
+
+}
